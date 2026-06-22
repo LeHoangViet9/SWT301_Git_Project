f5ac7911a3777cbbf5e2289b1fa0f7b898f6186d
TH <hiennthu205@gmail.com>
2026-06-22 11:42:17 +0700

    th_commit_accountrepository

---
+++ src/main/java/com/edu/fucarrentingsystem/repository/AccountRepository.java
@@ -0,0 +1,19 @@
+package com.edu.fucarrentingsystem.repository;
+
+import com.edu.fucarrentingsystem.model.entity.Account;
+import com.edu.fucarrentingsystem.model.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByAccountName(String accountName);
+
+    Optional<Account> findByEmail(String email);
+
+    List<Account> findByRole(Role role);
+}
